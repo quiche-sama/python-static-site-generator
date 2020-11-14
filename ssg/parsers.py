712b19cc5cc1c0a489fc14ec5fc5e8e89d8ada2d
to155756 <Davidian09>
2020-11-14 14:02:23 +0100

    Module 2 declaration of the parser class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,3 +4,16 @@ from pathlib import Path
 
 class Parser:
     extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        if extension in self.extensions:
+            return True
+        else:
+            return False
+
+    def parse(self, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
